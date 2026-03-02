6fdc1cae3392248d6c0d21da38d4906869cea181
wl3070-tech <wl3070@columbia.edu>
2026-03-01 19:42:48 -0500

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,22 @@
 import numpy as np
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    r = y / ppy
+
+    N = int(m * ppy)
+
+    t = np.arange(1, N + 1, dtype=float)
+
+    c = face * couponRate / ppy
+    cf = np.full(N, c, dtype=float)
+    cf[-1] = cf[-1] + face  # add principal at maturity
+
+    disc = (1 + r) ** t
+    pvcf = cf / disc
+
+    price = pvcf.sum()
+
+    duration_periods = (t * pvcf).sum() / price
+    duration_years = duration_periods / ppy
+
+    return duration_years
